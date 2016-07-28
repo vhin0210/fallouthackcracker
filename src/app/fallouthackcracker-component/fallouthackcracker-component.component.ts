26416d5fba82d85124d76ed0aacd211101315109
Mark Quirvien <vhin0210@gmail.com>
2016-07-28 10:03:47 +0800

    - update logic

--- src/app/fallouthackcracker-component/fallouthackcracker-component.component.ts
+++ src/app/fallouthackcracker-component/fallouthackcracker-component.component.ts
@@ -36,6 +36,7 @@ export class FallouthackcrackerComponentComponent implements OnInit {
   possibleAnswerLettersWords: FallouthackcrackerWordCharWords[] = [];
   wordsPoints: FallouthackcrackerWordPoints[] = [];
   onChange(words: string) {
+    words = words.toUpperCase();
     this.wordsArr = [];
     var wordsArr = words.split("\n");
     for (var key in wordsArr) {
@@ -61,21 +62,23 @@ export class FallouthackcrackerComponentComponent implements OnInit {
             words: []
           };
         }
-        tmpPossibleAnswerLettersWords.words.push(word);
+        tmpPossibleAnswerLettersWords.words[word.word] = word;
         this.possibleAnswerLettersWords[tmpIndexKey] = tmpPossibleAnswerLettersWords;
       }
     }
     for (var key in this.possibleAnswerLettersWords) {
       var letterWord = this.possibleAnswerLettersWords[key];
-      for (var word of letterWord.words) {
+      for (var keyWords in letterWord.words) {
+        var word: FallouthackcrackerWord = letterWord.words[keyWords];
         var tmpWordPoint: FallouthackcrackerWordPoints = this.wordsPoints[word.word];
         if (typeof tmpWordPoint == 'undefined') {
           tmpWordPoint = {
             word: word.word,
-            points: 0
+            points: 0,
+            likeness: null
           }
         }
-        tmpWordPoint.points += letterWord.words.length;
+        tmpWordPoint.points += Object.keys(letterWord.words).length;
         this.wordsPoints[word.word] = tmpWordPoint;
       }
     }
@@ -93,7 +96,144 @@ export class FallouthackcrackerComponentComponent implements OnInit {
 
       return 0;
     });
-    this.wordsPoints = tmpWordsPoints.slice(0, 4);
+    this.wordsPoints = tmpWordsPoints;
+  };
+  onCheckLikeness() {
+    var tmpWordsPoints: FallouthackcrackerWordPoints[] = [];
+    var tmpPossibleAnswerLettersWordsArr: FallouthackcrackerWordCharWords[] = [];
+    var tmpPossibleAnswerLettersWordsArr2: FallouthackcrackerWordCharWords[] = [];
+    for (var wordsPoint of this.wordsPoints) {
+      if (wordsPoint.likeness !== null || wordsPoint.likeness > 0) {
+        for (var i = 0, len = wordsPoint.word.length; i < len; i++) {
+          var tmpChar = wordsPoint.word[i];
+          var tmpIndexKey = i + ':' + tmpChar;
+          var tmpPossibleAnswerLettersWords: FallouthackcrackerWordCharWords = tmpPossibleAnswerLettersWordsArr[tmpIndexKey];
+          if (typeof tmpPossibleAnswerLettersWords == 'undefined') {
+            tmpPossibleAnswerLettersWords = {
+              char: tmpChar,
+              position: i + 1,
+              words: []
+            };
+          }
+          var tmpWord: FallouthackcrackerWord = {
+            word: wordsPoint.word,
+            likeness: wordsPoint.likeness
+          };
+          tmpPossibleAnswerLettersWords.words[tmpWord.word] = tmpWord;
+          tmpPossibleAnswerLettersWordsArr[tmpIndexKey] = tmpPossibleAnswerLettersWords;
+        }
+      }
+    }
+    for (var key in tmpPossibleAnswerLettersWordsArr) {
+      var letterWord: FallouthackcrackerWordCharWords = tmpPossibleAnswerLettersWordsArr[key];
+      for (var wordsPoint of this.wordsPoints) {
+        if (wordsPoint.likeness === null) {
+          for (var i = 0, len = wordsPoint.word.length; i < len; i++) {
+            var tmpChar = wordsPoint.word[i];
+            var tmpIndexKey = i + ':' + tmpChar;
+            var tmpPossibleAnswerLettersWords: FallouthackcrackerWordCharWords = tmpPossibleAnswerLettersWordsArr[tmpIndexKey];
+            if (typeof tmpPossibleAnswerLettersWords == 'undefined') {
+              tmpPossibleAnswerLettersWords = {
+                char: tmpChar,
+                position: i + 1,
+                words: []
+              };
+            }
+            var tmpWord: FallouthackcrackerWord = {
+              word: wordsPoint.word,
+              likeness: wordsPoint.likeness
+            };
+            tmpPossibleAnswerLettersWords.words[tmpWord.word] = tmpWord;
+            tmpPossibleAnswerLettersWordsArr[tmpIndexKey] = tmpPossibleAnswerLettersWords;
+          }
+        }
+      }
+    }
+    console.log(this.wordsPoints);
+    console.log(tmpPossibleAnswerLettersWordsArr);
+    // for (var key in tmpPossibleAnswerLettersWordsArr) {
+    //   var letterWord: FallouthackcrackerWordCharWords = tmpPossibleAnswerLettersWordsArr[key];
+    //   for (var keyWords in letterWord.words) {
+    //     var word: FallouthackcrackerWord = letterWord.words[keyWords];
+    //     var tmpWordPoint: FallouthackcrackerWordPoints = tmpPossibleAnswerLettersWordsArr[word.word];
+    //     if (typeof tmpWordPoint == 'undefined') {
+    //       tmpWordPoint = {
+    //         word: word.word,
+    //         points: 0,
+    //         likeness: word.likeness
+    //       }
+    //     }
+    //     if (word.likeness !== null) {
+    //       tmpWordPoint.points = 0;
+    //       for (var keyWords2 in letterWord.words) {
+    //         var word2: FallouthackcrackerWord = letterWord.words[keyWords2];
+    //         var tmpWordPoint2: FallouthackcrackerWordPoints = tmpPossibleAnswerLettersWordsArr[word2.word];
+    //         if (typeof tmpWordPoint2 == 'undefined') {
+    //           tmpWordPoint2 = {
+    //             word: word2.word,
+    //             points: 0,
+    //             likeness: word2.likeness
+    //           }
+    //         }
+    //         console.log(tmpWordPoint2, tmpWordPoint2.points);
+    //         tmpWordPoint2.points -= 1;
+    //         if (word2.word != word.word) {
+    //           console.log(tmpWordPoint2, tmpWordPoint2.points);
+    //           tmpWordsPoints[word2.word] = tmpWordPoint2;
+    //         }
+    //       }
+    //       tmpWordsPoints[word.word] = tmpWordPoint;
+    //     } else {
+    //       tmpWordPoint.points += 1;
+    //       tmpWordsPoints[word.word] = tmpWordPoint;
+    //     }
+    //   }
+    // }
+    for (var key in this.wordsPoints) {
+      var wordsPoint1: FallouthackcrackerWordPoints = this.wordsPoints[key];
+      if (wordsPoint1.likeness !== null) {
+        wordsPoint1.points = -99999999;
+      } else {
+        for (var i = 0, len = wordsPoint1.word.length; i < len; i++) {
+          var tmpChar = wordsPoint.word[i];
+          var tmpIndexKey = i + ':' + tmpChar;
+          var tmpPossibleAnswerLettersWords: FallouthackcrackerWordCharWords = tmpPossibleAnswerLettersWordsArr[tmpIndexKey];
+          for (var key2 in this.wordsPoints) {
+            var wordsPoint2: FallouthackcrackerWordPoints = this.wordsPoints[key2];
+            if (wordsPoint2.likeness !== null && typeof tmpPossibleAnswerLettersWords.words[wordsPoint2.word] != 'undefined') {
+              console.log(wordsPoint1.word, wordsPoint1.points, wordsPoint2.word);
+              wordsPoint1.points -= 1;
+              console.log(wordsPoint1.word, wordsPoint1.points, wordsPoint2.word);
+            }
+          }
+        }
+      }
+      tmpWordsPoints[wordsPoint1.word] = wordsPoint1;
+    }
+
+    var tmpWordsPoints2: FallouthackcrackerWordPoints[] = [];
+    for (var key in tmpWordsPoints) {
+      tmpWordsPoints2.push(tmpWordsPoints[key]);
+    }
+    tmpWordsPoints2.sort((n1:FallouthackcrackerWordPoints, n2:FallouthackcrackerWordPoints) => {
+      if (n1.points > n2.points) {
+        // if (n2.points == 0) {
+        //   return -1;
+        // }
+        return -1;
+      }
+      if (n1.points < n2.points) {
+        // if (n1.points == 0) {
+        //   return 1;
+        // }
+        return 1;
+      }
+
+      return 0;
+    });
+    if (tmpWordsPoints2.length > 0) {
+      this.wordsPoints = tmpWordsPoints2;
+    }
   };
 
   constructor() { }
--- src/app/fallouthackcracker-component/fallouthackcracker-word-points.ts
+++ src/app/fallouthackcracker-component/fallouthackcracker-word-points.ts
@@ -1,4 +1,5 @@
 export class FallouthackcrackerWordPoints {
 	word: string;
 	points: number;
+	likeness: number;
 }
